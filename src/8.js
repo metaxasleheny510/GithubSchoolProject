16a4fe619a862a8a708cc8f7e015bce3b11bf8b3
metaxasleheny510 <metaxasleheny510@gmail.com>
2025-03-12 12:26:17 +0000

    Add 8.js

---
+++ src/8.js
@@ -0,0 +1,7 @@
+// Create a function to generate a random number between 1 and 10
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+// Call the function and log the result to the console
+console.log(getRandomNumber());
