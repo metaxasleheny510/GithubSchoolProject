961bd26142cfc4338ca437617b5c5e6db26264de
metaxasleheny510 <metaxasleheny510@gmail.com>
2025-03-14 00:29:43 +0000

    Add 10.js

---
+++ src/10.js
@@ -0,0 +1,10 @@
+const getRandomCode = () => {
+  const numbers = "0123456789";
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  let code = "";
+  for (let i = 0; i < 8; i++) {
+    code += numbers[Math.floor(Math.random() * numbers.length)];
+    code += letters[Math.floor(Math.random() * letters.length)];
+  }
+  return code;
+};
