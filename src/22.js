e20022163cf88ca6e03b247575aebec80744970b
metaxasleheny510 <metaxasleheny510@gmail.com>
2025-03-30 08:59:55 +0000

    Add 22.js

---
+++ src/22.js
@@ -0,0 +1,8 @@
+// Example of JavaScript code for generating random data
+
+function getRandomInt(max) {
+  var maxNum = Math.floor(Math.random() * (max - 0 + 1)) + 0;
+  return maxNum;
+}
+
+console.log(getRandomInt(5));
