4a4583dd7446e547d9717403615169b332f2ebfa
metaxasleheny510 <metaxasleheny510@gmail.com>
2025-04-09 16:12:08 +0000

    Add 30.js

---
+++ src/30.js
@@ -0,0 +1,13 @@
+// Function to calculate the area of a rectangle
+function calculateRectangleArea(width, height) {
+  return width * height;
+}
+
+// Function to calculate the perimeter of a rectangle
+function calculateRectanglePerimeter(width, height) {
+  return 2 * (width + height);
+}
+
+// Main function to demonstrate Rectangle
+console.log("Rectangle Area:", calculateRectangleArea(10, 5));
+console.log("Rectangle Perimeter:", calculateRectanglePerimeter(10, 5));
