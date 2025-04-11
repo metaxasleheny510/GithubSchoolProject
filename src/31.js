0cc8327328b42baa2c00282b86bd5cf7db74d076
metaxasleheny510 <metaxasleheny510@gmail.com>
2025-04-11 11:45:20 +0000

    Add 31.js

---
+++ src/31.js
@@ -0,0 +1,13 @@
+// This is a simple example of a React component.
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>React Components</h1>
+        <p>Welcome to my React project!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
