d6afe32ecd25fabe9611d4359b09b8557e060574
metaxasleheny510 <metaxasleheny510@gmail.com>
2025-05-27 05:15:13 +0000

    Add 46.js

---
+++ src/46.js
@@ -0,0 +1,9 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+let fruits = ["apple", "banana", "orange"];
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
